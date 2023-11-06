28c9c8ae3171de6fcfa70f2a93ce4e2eeaf8413c
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2023-11-06 09:48:28 -0500

    API FRONT END

---
+++ api/frontend/script.js
@@ -0,0 +1,34 @@
+
+const urlAPI = "http://localhost/Desarrollo_Web_2_PHP/api/get.php";
+
+const tabla = document.getElementById("tablaDatos");
+
+const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.message)
+    cargarDatos(data.message);
+}
+
+
+
+const cargarDatos = (posts) => {
+    console.log(posts);
+    posts.forEach((post) => {
+        const {id_publicacion, titulo,contenido,autor,fecha_creacion} = post;
+        const contenedorFila = document.createElement('tr');
+        contenedorFila.innerHTML = `
+            <td><a href="detalle.html?id=${id_publicacion}">${id_publicacion}</a></td>
+            <td>${titulo}</td>
+            <td>${contenido}</td>
+            <td>${autor}</td>
+            <td>${fecha_creacion}</td>
+            <td><a href="editar.html?id=${id_publicacion}"> 📝</a></td>
+            <td><a href="eliminar.html?id=${id_publicacion}"> 🗑️</a></td>
+        `;
+        console.log(contenedorFila);
+        tabla.appendChild(contenedorFila);
+    });
+}
+
+getData(urlAPI);
